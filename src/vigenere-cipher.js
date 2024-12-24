0b1a804db214cb9f556e87d6033bba135e3dda14
ksenofonus <danilova.ka88@yandex.ru>
2024-12-24 16:24:49 +0400

    feat: add igenere cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,65 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(val) {
+    (val === false) ? this.reverse = false : this.reverse = true;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, keyword) {
+    if (message === undefined || keyword === undefined) throw new Error ('Incorrect arguments!');
+    let upperMessageArray = message
+			.toUpperCase()
+			.split('')
+			.map((el) => el.charCodeAt() - 65);
+    let length = upperMessageArray.filter((el) => el >= 0).length;
+    let upperKey = keyword
+			.padEnd(length, keyword)
+			.toUpperCase()
+			.split('')
+			.map((el) => el.charCodeAt() - 65);
+    let j = -1;
+		let result = upperMessageArray.map((el, i) => {
+			if (el >= 0 && el <= 25) {
+				j += 1;
+				return ((el + upperKey[j]) % 26) + 65;
+			} else {
+				return el + 65;
+			}
+		});
+    return this.reverse === true
+			? result.map((el) => String.fromCharCode(el)).join('')
+			: result
+					.map((el) => String.fromCharCode(el))
+					.reverse()
+					.join('');
+  }
+  decrypt(cipher, keyword) {
+    if (cipher === undefined || keyword === undefined)
+			throw new Error('Incorrect arguments!');
+    let upperMessageArray = cipher
+			.toUpperCase()
+			.split('')
+			.map((el) => el.charCodeAt() - 65);
+		let length = upperMessageArray.filter((el) => el >= 0).length;
+		let upperKey = keyword
+			.padEnd(length, keyword)
+			.toUpperCase()
+			.split('')
+			.map((el) => el.charCodeAt() - 65);
+		let j = -1;
+		let result = upperMessageArray.map((el, i) => {
+			if (el >= 0 && el <= 25) {
+				j += 1;
+				if (el >= upperKey[j]) return ((el - upperKey[j]) % 26) + 65
+				else return ((el - upperKey[j]) % 26) + 26 + 65;
+			} else {
+				return el + 65;
+			}
+		});
+		return this.reverse === true
+			? result.map((el) => String.fromCharCode(el)).join('')
+			: result.map((el) => String.fromCharCode(el))
+        .reverse()
+        .join('');
   }
 }
 
